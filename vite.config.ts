63c6bc2b6f5495ca2cb0508b5dd96561e2d25dd5
eskialexis7 <eskialexis7@gmail.com>
2026-04-28 23:05:18 +0300

    LASTTTTTT

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   base: "/digitalvitrina-glow/",
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
